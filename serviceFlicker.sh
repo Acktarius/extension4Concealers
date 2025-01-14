ff02b7fa5b505553d0662fadb639c68f76d2b7dd
acktarius <xchaps@aol.com>
2025-01-14 18:02:21 -0500

    logo

--- serviceFlicker.sh
+++ serviceFlicker.sh
@@ -2,15 +2,54 @@
 # this file is subject to Licence
 #Copyright (c) 2025, Acktarius
 ###################################
+#Couleurs
+case "$TERM" in
+        xterm-256color)
+        WHITE=$(tput setaf 7 bold)
+        ORANGE=$(tput setaf 202)
+        GRIS=$(tput setaf 245)
+		LINK=$(tput setaf 4 smul)
+        TURNOFF=$(tput sgr0)
+        ;;
+        *)
+        WHITE=''
+		ORANGE=''
+        GRIS=''
+		LINK=''
+        TURNOFF=''
+        ;;
+esac
+#Presentation
+presentation (){
+echo -e "${GRIS}####################################################################"
+echo -e "#                                                                  #"
+echo -e "#-->   ${WHITE}     SERVICE FLICKER FOR ${ORANGE}ccx-mining.service   ${TURNOFF}${GRIS}           <--#"
+echo -e "#                                                                  #"
+echo -e "#                                                                  #"
+echo -e "#                                                                  #"
+echo -e "####################################################    ${WHITE}.::::."
+echo -e "${GRIS}#                                                   ${WHITE}.:---=--=--::."
+echo -e "${GRIS}#${WHITE}  Enter your password to ${ORANGE}${1} ${2} ${WHITE}service \t    -=:+-.  .-=:=:"
+echo -e "${GRIS}#   or ${WHITE}Ctrl+C ${GRIS}to abandon                    \t    -=:+."
+echo -e "${GRIS}#                                                   ${WHITE}-=:+."
+echo -e "${GRIS}#                                                   ${WHITE}-=:+."
+echo -e "${GRIS}#						    ${WHITE}-=:=."
+echo -e "${GRIS}#                                                   ${WHITE}-+:-:    .::."
+echo -e "${GRIS}#						    ${WHITE}-+==------===-"
+echo -e "${GRIS}########                                               ${WHITE}:-=-==-:${TURNOFF}"
+
+}
+
+
 serviceFlicker () {
 case $1 in
 	"1")
-	echo "Enter your password to stop ${2} service or Ctrl+C to abandon"
+	presentation "stop" ${2}
 	sudo systemctl stop ccx-${2}.service
 	exit 0
 	;;
 	"0")
-	echo "Enter your password to start ${2} service or Ctrl+C to abandon"
+	presentation "start" ${2}
 	sudo systemctl restart ccx-${2}.service
 	exit 0
 	;;
@@ -21,4 +60,3 @@ case $1 in
 esac
 }
 serviceFlicker $1 $2
-
